342053decc54b8fcb243d323e26853c611a15d29
Khoa Hoang <whoskhoahoang@gmail.com>
2020-06-22 15:39:28 +0900

    Implement Send in hello_world
    Also add execution_notes.txt to
    top-level project location.

--- hello_world/src/main/java/com/khoa/hello_world/Send.java
+++ hello_world/src/main/java/com/khoa/hello_world/Send.java
@@ -8,8 +8,16 @@ public class Send {
 
   private final static String QUEUE_NAME = "hello";
 
-  public static void main(String[] args) {
-    System.out.println("Test123");
+  public static void main(String[] args) throws Exception {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    try (Connection connection = factory.newConnection();
+         Channel channel = connection.createChannel()) {
+      channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+      String message = "Hello world!";
+      channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+      System.out.println(" [x] Sent '" + message + "'");
+    }
   }
 
 }
