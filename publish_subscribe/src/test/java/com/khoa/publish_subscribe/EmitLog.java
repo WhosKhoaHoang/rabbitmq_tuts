31c0cdb42d7a53d2326b3de7a0cce4f9c0ce43de
Khoa Hoang <whoskhoahoang@gmail.com>
2020-06-23 18:25:15 +0900

    Add comments in publish_subscribe

--- publish_subscribe/src/test/java/com/khoa/publish_subscribe/EmitLog.java
+++ publish_subscribe/src/test/java/com/khoa/publish_subscribe/EmitLog.java
@@ -10,6 +10,24 @@ public class EmitLog {
   private static final String EXCHANGE_NAME = "logs";
 
   public static void main(String[] args) throws Exception {
+    // EmitLog is tied to ReceiveLogs through the
+    // common exchange that they both connect to
+    // - Basically, the flow of this example is that ReceiveLogs
+    //   creates a queue (which doesn't need to be named) that is
+    //   bound to the same exchange established here in EmitLog
+    //   (which we've called "logs"). EmitLog then sends messages
+    //   TO THE EXCHANGE, which then gets sent to all the queues
+    //   within the exchange due to its type being "fanout".
+    // - If no queue is bound to the "logs" exchange, then any messages
+    //   sent by EmitLog here are lost. Since ReceiveLogs binds a queue to
+    //   the "logs" exchange, you need to run ReceiveLogs first if you expect
+    //   those messages to go anywhere. Also, run multiple instances of
+    //   ReceiveLogs to see how multiple queues get bound to the "logs"
+    //   exchange and how they all receive messages from EmitLog.
+    // - Be sure to enable "Allow Parallel Runs" for ReceiveLogs' run configs
+    //   so that you can run multiple instances of ReceiveLogs and get a better
+    //   sense of how this pattern works.
+
     ConnectionFactory factory = new ConnectionFactory();
     factory.setHost("localhost");
     try (Connection connection = factory.newConnection();
--- publish_subscribe/src/test/java/com/khoa/publish_subscribe/ReceiveLogs.java
+++ publish_subscribe/src/test/java/com/khoa/publish_subscribe/ReceiveLogs.java
@@ -11,13 +11,20 @@ public class ReceiveLogs {
   private static final String EXCHANGE_NAME = "logs";
 
   public static void main(String[] args) throws Exception {
+    // EmitLog is tied to ReceiveLogs through the
+    // common exchange that they both connect to
+
     ConnectionFactory factory = new ConnectionFactory();
     factory.setHost("localhost");
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
     channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+    // When we supply no parameters to queueDeclare() we create
+    // a non-durable, exclusive, autodelete queue with a generated name:
     String queueName = channel.queueDeclare().getQueue();
+
+    // The relationship between an exchange and a queue is called a binding
     channel.queueBind(queueName, EXCHANGE_NAME, "");
 
     System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
